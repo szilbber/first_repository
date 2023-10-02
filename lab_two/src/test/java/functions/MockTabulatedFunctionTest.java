91cb5eadb3013dd77b8436ef3aa0da6fe3f23506
CrucifirldOnTheCross <toooriiuum@gmail.com>
2023-10-02 17:25:27 +0400

    Mock object and Test

---
+++ lab_two/src/test/java/functions/MockTabulatedFunctionTest.java
@@ -0,0 +1,17 @@
+package functions;
+
+import junit.framework.TestCase;
+
+public class MockTabulatedFunctionTest extends TestCase {
+    MockTabulatedFunction obj = new MockTabulatedFunction();
+
+    public void testInterpolate() {
+        assertEquals(0., obj.interpolate(1, 1, 0, 0, 1));
+    }
+    public void testApply(){
+        assertEquals(1.0, obj.apply(0));
+        assertEquals(0.0, obj.apply(1));
+        assertEquals(-1.0, obj.apply(2));
+        assertEquals(2.0, obj.apply(-1));
+    }
+}
