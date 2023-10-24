83e515846cb7892efb5161a6766d0b5d1b42cfe6
szilbber <Sofya.dish@gmail.com>
2023-10-25 01:01:59 +0400

    2.1X

---
+++ lab_two/src/main/java/operations/TabulatedDifferentialOperator.java
@@ -0,0 +1,47 @@
+package operations;
+import functions.Point;
+import functions.TabulatedFunction;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.TabulatedFunctionFactory;
+
+public class TabulatedDifferentialOperator implements DifferentialOperator<TabulatedFunction> {
+    TabulatedFunctionFactory factory;
+
+    TabulatedDifferentialOperator(TabulatedFunctionFactory factory){
+
+    }
+
+    TabulatedDifferentialOperator(){
+        factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public double apply(double x) {
+        return 0;
+    }
+
+    @Override
+    public TabulatedFunction derive(TabulatedFunction function) {
+        Point[] points = TabulatedFunctionOperationService.asPoints(function);
+        int size = points.length;
+        double xValues [] = new double[size];
+        double yValues [] = new double[size];
+        int index = 0;
+        while(index != size-1){
+            xValues[index] = points[index].x;
+            yValues[index] = (points[index+1].y - points[index].y)/(points[index+1].x - points[index].x);
+        }
+        xValues[size-1] = points[size-2].x;
+        yValues[size-1] = yValues[size-2];
+        return factory.create(xValues, yValues);
+    }
+
+}
---
+++ lab_two/src/test/java/operations/TabulatedDifferentialOperatorTest.java
@@ -0,0 +1,7 @@
+package operations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TabulatedDifferentialOperatorTest {
+
+}
